47708641a49c3f53989e5c0cdcdcdc727cc685c0
Patryk Nowak <nowak.p92@gmail.com>
2017-01-30 10:54:46 +0100

    added app_timer

---
+++ src/config/app_timer_config.h
@@ -0,0 +1,14 @@
+/*
+ * app_timer_config.h
+ */
+
+#ifndef APP_TIMER_CONFIG_H_
+#define APP_TIMER_CONFIG_H_
+#include <stddef.h>
+
+
+#define APP_TIMER_PRESCALER              0
+#define APP_TIMER_OP_QUEUE_SIZE          10
+#define APP_TIMER_SCHEDULER_FUNC         NULL
+
+#endif /* APP_TIMER_CONFIG_H_ */
--- src/main.c
+++ src/main.c
@@ -6,10 +6,13 @@
 #include "nrf_gpio.h"
 #include "nrf_delay.h"
 #include "bluetooth.h"
+#include "app_timer.h"
+#include "app_timer_config.h"
 
 
 int main(void)
 {
+	APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_OP_QUEUE_SIZE, APP_TIMER_SCHEDULER_FUNC);
 	ble_stack_init();
 
 	nrf_gpio_cfg_output(21);
